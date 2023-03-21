7345eb21bd7c11456f50e2d2fcaeba1624e752e2
System Administrator <root@Dannyyusis-MacBook-Pro.local>
2023-03-21 20:31:10 +0100

    Adjusts

--- treffio_vuejs/src/components/Cards/TaskComponent.vue
+++ treffio_vuejs/src/components/Cards/TaskComponent.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="card w-25 task-card" :style="getIndex" >
         <div class="card-body text-center">
-            <div class="text-start">
-                <i @mouseover="showDeadline(card_deadline)" v-if="card_deadline" class="fa-solid fa-exclamation fa-beat-fade position-absolute fs-3"></i>
+            <div class="text-start" data-toggle="tooltip" :title="card_deadline">
+                <i v-if="card_deadline" class="fa-solid fa-exclamation fa-beat-fade position-absolute fs-3"></i>
             </div>
             <span class="card-number fw-bolder">{{ card_number }}</span>
             <h3 class="card-title">{{ card_title }}</h3>
@@ -21,11 +21,6 @@ export default {
         card_title: String,
         card_description: String,
         card_deadline: String
-    },
-    methods: {
-        showDeadline() {
-
-        }
     },
     computed: {
         ...mapGetters([
--- treffio_vuejs/src/views/ListTasks.vue
+++ treffio_vuejs/src/views/ListTasks.vue
@@ -4,6 +4,7 @@
                     <div class="text-start">
                         <h2>New Tasks</h2>
                     </div>
+                    <hr>
                     <div class="tasks-list d-flex gap-3 pt-3">
                         <TaskComponent :card_number="cards.card1.card_number" :card_title="cards.card1.card_title" :card_description="cards.card1.card_description"/>
                         <TaskComponent :card_number="cards.card2.card_number" :card_title="cards.card2.card_title" :card_description="cards.card2.card_description"/>
@@ -13,18 +14,20 @@
                     <div class="text-start">
                         <h2>Deadlines</h2>
                     </div>
+                    <hr>
                     <div class="tasks-list d-flex gap-3 pt-3">
-                        <TaskComponent :card_number="cards.card1.card_number" :card_title="cards.card1.card_title" :card_description="cards.card1.card_description"/>
-                        <TaskComponent card_deadline="13 Mar 2023" :card_number="cards.card2.card_number" :card_title="cards.card2.card_title" :card_description="cards.card2.card_description"/>
+                        <TaskComponent :card_number="cards.card3.card_number" :card_title="cards.card3.card_title" :card_description="cards.card3.card_description" :card_deadline="cards.card3.card_deadline"/>
                     </div>
                 </div>
                 <div class="all-tasks ps-4 mt-5 mb-5" :style="getIndex">
                     <div class="text-start">
-                        <h2>All Tasks</h2>
+                        <h2>On Going Tasks</h2>
                     </div>
+                    <hr>
                     <div class="tasks-list d-flex gap-3 pt-3">
                         <TaskComponent :card_number="cards.card1.card_number" :card_title="cards.card1.card_title" :card_description="cards.card1.card_description"/>
                         <TaskComponent :card_number="cards.card2.card_number" :card_title="cards.card2.card_title" :card_description="cards.card2.card_description"/>
+                        <TaskComponent :card_number="cards.card3.card_number" :card_title="cards.card3.card_title" :card_description="cards.card3.card_description" :card_deadline="cards.card3.card_deadline"/>
                     </div>
                 </div>
             </div>
@@ -48,6 +51,12 @@ export default {
                     card_number: 2,
                     card_title: "Clean Room",
                     card_description: "I need to clean the room before my mom comes home."
+                },
+                card3:{
+                    card_number: 3,
+                    card_title: "Walk the dog",
+                    card_description: "I need to walk the dog before my mom comes home.",
+                    card_deadline: "13 Mar 2023 | 10:11 a.m"
                 }
             }
         }
