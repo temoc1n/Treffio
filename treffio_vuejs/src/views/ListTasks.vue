068f06d166b34a08a0ef3c6e0c5e004f95f1368e
System Administrator <root@Dannyyusis-MacBook-Pro.local>
2023-03-21 21:06:59 +0100

    Final changes Task List

--- treffio_vuejs/src/views/ListTasks.vue
+++ treffio_vuejs/src/views/ListTasks.vue
@@ -1,36 +1,41 @@
 <template>
    <div class="container-fluid">
-                <div class="new-tasks ps-4 mt-5">
-                    <div class="text-start">
-                        <h2>New Tasks</h2>
-                    </div>
-                    <hr>
-                    <div class="tasks-list d-flex gap-3 pt-3">
-                        <TaskComponent :card_number="cards.card1.card_number" :card_title="cards.card1.card_title" :card_description="cards.card1.card_description"/>
-                        <TaskComponent :card_number="cards.card2.card_number" :card_title="cards.card2.card_title" :card_description="cards.card2.card_description"/>
-                    </div>
+        <div class="row">
+            <div class="new-tasks ps-4 mt-5">
+                <div class="text-start">
+                    <h2>New Tasks</h2>
                 </div>
-                <div class="deadline-tasks ps-4 mt-5">
-                    <div class="text-start">
-                        <h2>Deadlines</h2>
-                    </div>
-                    <hr>
-                    <div class="tasks-list d-flex gap-3 pt-3">
-                        <TaskComponent :card_number="cards.card3.card_number" :card_title="cards.card3.card_title" :card_description="cards.card3.card_description" :card_deadline="cards.card3.card_deadline"/>
-                    </div>
-                </div>
-                <div class="all-tasks ps-4 mt-5 mb-5" :style="getIndex">
-                    <div class="text-start">
-                        <h2>On Going Tasks</h2>
-                    </div>
-                    <hr>
-                    <div class="tasks-list d-flex gap-3 pt-3">
-                        <TaskComponent :card_number="cards.card1.card_number" :card_title="cards.card1.card_title" :card_description="cards.card1.card_description"/>
-                        <TaskComponent :card_number="cards.card2.card_number" :card_title="cards.card2.card_title" :card_description="cards.card2.card_description"/>
-                        <TaskComponent :card_number="cards.card3.card_number" :card_title="cards.card3.card_title" :card_description="cards.card3.card_description" :card_deadline="cards.card3.card_deadline"/>
-                    </div>
+                <hr>
+                <!--Only the first 3 new tasks-->
+                <div class="tasks-list d-flex gap-3 pt-3">
+                    <TaskComponent :card_number="cards.card1.card_number" :card_title="cards.card1.card_title" :card_description="cards.card1.card_description"/>
+                    <TaskComponent :card_number="cards.card2.card_number" :card_title="cards.card2.card_title" :card_description="cards.card2.card_description"/>
                 </div>
             </div>
+            <div class="deadline-tasks ps-4 mt-5">
+                <div class="text-start">
+                    <h2>Deadlines</h2>
+                </div>
+                <hr>
+                <div class="tasks-list d-flex gap-3 pt-3">
+                    <TaskComponent :card_number="cards.card3.card_number" :card_title="cards.card3.card_title" :card_description="cards.card3.card_description" :card_deadline="cards.card3.card_deadline"/>
+                </div>
+            </div>
+            <div class="all-tasks ps-4 mt-5 mb-5" :style="getIndex">
+                <div class="text-start">
+                    <h2>On Going Tasks</h2>
+                </div>
+                <hr>
+                <div class="tasks-list d-flex gap-3 pt-3">
+                    <TaskComponent :card_number="cards.card1.card_number" :card_title="cards.card1.card_title" :card_description="cards.card1.card_description"/>
+                    <TaskComponent :card_number="cards.card2.card_number" :card_title="cards.card2.card_title" :card_description="cards.card2.card_description"/>
+                    <TaskComponent :card_number="cards.card3.card_number" :card_title="cards.card3.card_title" :card_description="cards.card3.card_description" :card_deadline="cards.card3.card_deadline"/>
+                    <TaskComponent :card_number="cards.card3.card_number" :card_title="cards.card3.card_title" :card_description="cards.card3.card_description" :card_deadline="cards.card3.card_deadline"/>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 import TaskComponent from '@/components/Cards/TaskComponent.vue'
