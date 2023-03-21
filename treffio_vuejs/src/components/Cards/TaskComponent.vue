5a010624da46c33f1084486a9237902b91965979
System Administrator <root@Dannyyusis-MacBook-Pro.local>
2023-03-21 20:19:06 +0100

    Added style to other components

--- treffio_vuejs/src/components/Cards/TaskComponent.vue
+++ treffio_vuejs/src/components/Cards/TaskComponent.vue
@@ -1,18 +1,32 @@
 <template>
     <div class="card w-25 task-card" :style="getIndex" >
-  <div class="card-body text-center">
-    <span class="card-number fw-bolder">1</span>
-    <h3 class="card-title">Card title</h3>
-    <p class="card-text">With supporting text below as a natural lead-in g text below as a natural lead to additional content. </p>
-    <a href="#" class="btn delete-btn btn-outline-light me-3">Delete</a>
-    <a href="#" class="btn complete-btn btn-outline-light">Completed!</a>
-  </div>
-</div>
+        <div class="card-body text-center">
+            <div class="text-start">
+                <i @mouseover="showDeadline(card_deadline)" v-if="card_deadline" class="fa-solid fa-exclamation fa-beat-fade position-absolute fs-3"></i>
+            </div>
+            <span class="card-number fw-bolder">{{ card_number }}</span>
+            <h3 class="card-title">{{ card_title }}</h3>
+            <p class="card-text">{{ card_description }}</p>
+            <a href="#" class="btn delete-btn btn-outline-light me-3">Delete</a>
+            <a href="#" class="btn complete-btn btn-outline-light">Completed!</a>
+        </div>
+    </div>
 </template>
 <script>
 import { mapGetters } from 'vuex';
 export default {
     name: 'TaskComponent',
+    props: {
+        card_number: Number,
+        card_title: String,
+        card_description: String,
+        card_deadline: String
+    },
+    methods: {
+        showDeadline() {
+
+        }
+    },
     computed: {
         ...mapGetters([
             'getIndex'
--- treffio_vuejs/src/views/ListTasks.vue
+++ treffio_vuejs/src/views/ListTasks.vue
@@ -5,17 +5,26 @@
                         <h2>New Tasks</h2>
                     </div>
                     <div class="tasks-list d-flex gap-3 pt-3">
-                        <TaskComponent />
+                        <TaskComponent :card_number="cards.card1.card_number" :card_title="cards.card1.card_title" :card_description="cards.card1.card_description"/>
+                        <TaskComponent :card_number="cards.card2.card_number" :card_title="cards.card2.card_title" :card_description="cards.card2.card_description"/>
                     </div>
                 </div>
                 <div class="deadline-tasks ps-4 mt-5">
                     <div class="text-start">
                         <h2>Deadlines</h2>
                     </div>
+                    <div class="tasks-list d-flex gap-3 pt-3">
+                        <TaskComponent :card_number="cards.card1.card_number" :card_title="cards.card1.card_title" :card_description="cards.card1.card_description"/>
+                        <TaskComponent card_deadline="13 Mar 2023" :card_number="cards.card2.card_number" :card_title="cards.card2.card_title" :card_description="cards.card2.card_description"/>
+                    </div>
                 </div>
-                <div class="all-tasks ps-4 mt-5" :style="getIndex">
+                <div class="all-tasks ps-4 mt-5 mb-5" :style="getIndex">
                     <div class="text-start">
                         <h2>All Tasks</h2>
+                    </div>
+                    <div class="tasks-list d-flex gap-3 pt-3">
+                        <TaskComponent :card_number="cards.card1.card_number" :card_title="cards.card1.card_title" :card_description="cards.card1.card_description"/>
+                        <TaskComponent :card_number="cards.card2.card_number" :card_title="cards.card2.card_title" :card_description="cards.card2.card_description"/>
                     </div>
                 </div>
             </div>
@@ -27,6 +36,22 @@ export default {
     components:{
         'TaskComponent': TaskComponent
     },
+    data() {
+        return{
+            cards:{
+                card1:{
+                    card_number: 1,
+                    card_title: "Wash Dishes",
+                    card_description: "I need to wash the dishes before my mom comes home."
+                },
+                card2:{
+                    card_number: 2,
+                    card_title: "Clean Room",
+                    card_description: "I need to clean the room before my mom comes home."
+                }
+            }
+        }
+    }
 }
 </script>
 <style scoped>
